820f0c9924061b6eff9905e745b03eac349af6a6
3502210569_MithilaR <rmithila786@gmail.com>
2025-02-04 13:52:17 +0530

    BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main{
+class BinarySearch{
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         int[] arr = {1, 2, 3, 4, 5};
